7316aa5c468e2ceb45bf8cb47c8e492e0ea72d47
Zeeshan <mzeeshi187@gmail.com>
2024-10-17 03:58:06 -0700

    footer updated

--- recipe.js
+++ recipe.js
@@ -1,29 +1,23 @@
 let inputValue = document.getElementById("ingredient_input");
-let ingredienrt_one =document.getElementById('ingredienrt_one')
-let ingredienrt_two =document.getElementById('ingredienrt_two')
-let ingredienrt_three =document.getElementById('ingredienrt_three')
-let ingredienrt_four =document.getElementById('ingredienrt_four')
-let ingredienrt_five =document.getElementById('ingredienrt_five')
-
+let ingredienrt_one = document.getElementById("ingredienrt_one");
+let ingredienrt_two = document.getElementById("ingredienrt_two");
+let ingredienrt_three = document.getElementById("ingredienrt_three");
+let ingredienrt_four = document.getElementById("ingredienrt_four");
+let ingredienrt_five = document.getElementById("ingredienrt_five");
 
 function UpdateIngredient(event) {
-    if (event) event.preventDefault();
-    if(document.getElementById("ingredient_input").value<=0){
-       
-    }
-    else{
-        let first= 600*inputValue.value;
-    let second= 2*inputValue.value;
-    let third= 1*inputValue.value;
-    let fourth= 1*inputValue.value;
-    let ffith= 80*inputValue.value;
+  if (event) event.preventDefault();
+    let first = 600 * inputValue.value;
+    let second = 2 * inputValue.value;
+    let third = 1 * inputValue.value;
+    let fourth = 1 * inputValue.value;
+    let ffith = 80 * inputValue.value;
 
-    ingredienrt_one.textContent= `${first} g`
-     ingredienrt_two.textContent= `${second}`
-     ingredienrt_three.textContent= `${third} EL`
-     ingredienrt_four.textContent= `${fourth} `
-     ingredienrt_five.textContent= `${ffith} g`
-    }
+    ingredienrt_one.textContent = `${first} g`;
+    ingredienrt_two.textContent = `${second}`;
+    ingredienrt_three.textContent = `${third} EL`;
+    ingredienrt_four.textContent = `${fourth} `;
+    ingredienrt_five.textContent = `${ffith} g`;
 }
 
 UpdateIngredient();
